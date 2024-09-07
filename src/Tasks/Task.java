775d48bf75261ddee05bf391d4e936bd3b411b5b
Renzen707 <renzen707@vk.com>
2024-09-08 01:41:59 +0500

    Added setters, getters, override equals and hashcode

--- src/Tasks/Task.java
+++ src/Tasks/Task.java
@@ -1,4 +1,66 @@
 package Tasks;
 
+import java.util.Objects;
+
 public class Task {
+    protected int id;
+    protected String taskName;
+    protected Status status;
+    protected String description;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && Objects.equals(taskName, task.taskName) && status == task.status && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, taskName, status, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", taskName='" + taskName + '\'' +
+                ", status=" + status +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
